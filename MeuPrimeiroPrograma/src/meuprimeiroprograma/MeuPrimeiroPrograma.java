d63dd5785619ee6baec11de8a45678447479733e
beritcarvalho <56369664+beritcarvalho@users.noreply.github.com>
2020-11-02 18:02:29 -0300

    criacao do primeiro programa em java

---
+++ MeuPrimeiroPrograma/src/meuprimeiroprograma/MeuPrimeiroPrograma.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meuprimeiroprograma;
+
+/**
+ *
+ * @author Sandra
+ */
+public class MeuPrimeiroPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Berit Carvalho é casado com ");
+        String nome = "Sandra";
+        String sobrenome = "Silva";
+        
+        System.out.print(nome + " " + sobrenome);
+        
+    }
+    
+}
